1fd852fb46cdc746c084a2ec09645ba82c3c9abc
prashantjd79 <prashant.rytclick@gmail.com>
2025-03-27 18:25:15 +0530

    yyyy

--- src/routes/mentorRoutes.js
+++ src/routes/mentorRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { registerMentor, loginMentor,getSubmittedAssignments,gradeAssignment } = require("../controllers/mentorController");
-const { protectMentor } = require("../middleware/authMiddleware");
+const { protectMentor, adminProtect } = require("../middleware/authMiddleware");
 const uploadMentorPhoto = require("../middleware/uploadMentorPhoto");
 const router = express.Router();
 
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/signup", uploadMentorPhoto.single("photo"), registerMentor);
 
 router.post("/login", loginMentor);
-router.get("/submitted-assignments", protectMentor, getSubmittedAssignments);
+router.get("/submitted-assignments",adminProtect, protectMentor, getSubmittedAssignments);
 router.post("/grade-assignment", protectMentor, gradeAssignment);
 
 module.exports = router;
