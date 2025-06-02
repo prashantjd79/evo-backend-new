47fbb7235721e317b36c0e57083d91fba63c0234
prashantjd79 <prashant.rytclick@gmail.com>
2025-06-02 16:20:59 +0530

    y

--- src/utils/server.js
+++ src/utils/server.js
@@ -119,5 +119,5 @@ app.use("/uploads", express.static("uploads"));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
